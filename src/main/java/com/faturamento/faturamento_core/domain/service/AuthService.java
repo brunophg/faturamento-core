e0d235033f142d39ef84b06412e46c612095c391
brunophg <brunophgjf@gmail.com>
2026-07-21 17:27:30 -0300

    feat: add AuthService

--- src/main/java/com/faturamento/faturamento_core/domain/service/AuthService.java
+++ src/main/java/com/faturamento/faturamento_core/domain/service/AuthService.java
@@ -1,4 +1,30 @@
 package com.faturamento.faturamento_core.domain.service;
 
-public class AuthService {
+import com.faturamento.faturamento_core.domain.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AuthService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserDetails usuario = usuarioRepository.findByLogin(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuario não encontrado: " + username);
+        }
+
+        return usuario;
+    }
 }
