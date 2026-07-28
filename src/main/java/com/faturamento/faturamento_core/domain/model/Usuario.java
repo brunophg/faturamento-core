c73a2ba49eb7daf664d84b1e48136aa19ba80064
brunophg <brunophgjf@gmail.com>
2026-07-28 16:50:03 -0300

    fix: User entity

--- src/main/java/com/faturamento/faturamento_core/domain/model/Usuario.java
+++ src/main/java/com/faturamento/faturamento_core/domain/model/Usuario.java
@@ -26,7 +26,6 @@ public class Usuario implements UserDetails {
     }
 
     public Usuario(String login, String senha) {
-        // Removido o this.id = id; pois o banco gera o ID automaticamente
         this.login = login;
         this.senha = senha;
     }
@@ -43,50 +42,39 @@ public class Usuario implements UserDetails {
         return senha;
     }
 
-    // =========================================================
-    // MÉTODOS DO USERDETAILS (O QUE O SPRING SECURITY LÊ)
-    // =========================================================
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // Por enquanto, todos têm o mesmo nível de acesso.
-        // No futuro, se houver "ADMIN" e "VENDEDOR", a regra entra aqui.
         return List.of();
     }
 
     @Override
     public String getPassword() {
-        return this.senha; // APONTAMENTO CORRETO PARA A SENHA
+        return this.senha;
     }
 
     @Override
     public String getUsername() {
-        return this.login; // APONTAMENTO CORRETO PARA O LOGIN
+        return this.login;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true; // Conta não expira
-    }
+        return true; }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true; // Conta não está bloqueada
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true; // Senha não expira
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return true; // Usuário está ativo
+        return true;
     }
-
-    // =========================================================
-    // EQUALS & HASHCODE
-    // =========================================================
 
     @Override
     public boolean equals(Object o) {
