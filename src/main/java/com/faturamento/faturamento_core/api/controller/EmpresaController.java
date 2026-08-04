7b00f1f5e83a9916ee6c95f7c3c53e5fa16a15ef
brunophg <brunophgjf@gmail.com>
2026-08-04 13:52:11 -0300

    feat: add listarTodos and buscarPorId endpoints to EmpresaController

---
+++ src/main/java/com/faturamento/faturamento_core/api/controller/EmpresaController.java
@@ -0,0 +1,37 @@
+package com.faturamento.faturamento_core.api.controller;
+
+import com.faturamento.faturamento_core.domain.dto.empresa.EmpresaResponseDTO;
+import com.faturamento.faturamento_core.domain.model.Empresa;
+import com.faturamento.faturamento_core.domain.repository.EmpresaRepository;
+import com.faturamento.faturamento_core.domain.service.EmpresaService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/empresas")
+public class EmpresaController {
+
+    private final EmpresaService empresaService;
+
+    public EmpresaController(EmpresaService empresaService) {
+        this.empresaService = empresaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmpresaResponseDTO>> listarTodas(){
+        List<EmpresaResponseDTO> lista = empresaService.listarTodos();
+
+        return ResponseEntity.ok().body(lista);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmpresaResponseDTO> buscarPorId(@PathVariable long id) {
+        EmpresaResponseDTO empresa = empresaService.buscarPorId(id);
+
+        return ResponseEntity.ok().body(empresa);
+    }
+}
