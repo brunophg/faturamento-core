cf623b380a1900de27eae900ee1063dcdd796880
brunophg <brunophgjf@gmail.com>
2026-08-13 16:20:06 -0300

    feat: add operation findByEmpresaEmisoraId to repository for NotaFiscal

--- src/main/java/com/faturamento/faturamento_core/domain/repository/NotaFiscalRepository.java
+++ src/main/java/com/faturamento/faturamento_core/domain/repository/NotaFiscalRepository.java
@@ -3,8 +3,12 @@ package com.faturamento.faturamento_core.domain.repository;
 import com.faturamento.faturamento_core.domain.model.NotaFiscal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotaFiscalRepository extends JpaRepository<NotaFiscal, Long> {
 
     // RN1 Evitar duplicidade.
     boolean existsByNumeroNotaAndEmpresaEmissoraId(Long numeroNota, Long empresaId);
+
+    List<NotaFiscal> findByEmpresaEmissoraId(Long empresaId);
 }
