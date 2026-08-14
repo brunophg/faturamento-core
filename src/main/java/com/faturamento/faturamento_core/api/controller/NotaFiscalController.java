5db2cf0cea2bd720ae1ae92c4068a667cd18557b
brunophg <brunophgjf@gmail.com>
2026-08-14 10:56:56 -0300

    implements  endpoint POST to NotaFiscalController

--- src/main/java/com/faturamento/faturamento_core/api/controller/NotaFiscalController.java
+++ src/main/java/com/faturamento/faturamento_core/api/controller/NotaFiscalController.java
@@ -1,14 +1,15 @@
 package com.faturamento.faturamento_core.api.controller;
 
+import com.faturamento.faturamento_core.domain.dto.notafiscal.NotaFiscalRequestDTO;
 import com.faturamento.faturamento_core.domain.dto.notafiscal.NotaFiscalResponseDTO;
 import com.faturamento.faturamento_core.domain.service.NotaFiscalService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,6 +27,19 @@ public class NotaFiscalController {
         List<NotaFiscalResponseDTO> notas = notaFiscalService.buscarPorEmpresa(empresaId);
 
         return ResponseEntity.status(HttpStatus.OK).body(notas);
+    }
+
+    @PostMapping
+    public ResponseEntity<NotaFiscalResponseDTO> emitir(@RequestBody @Valid NotaFiscalRequestDTO request) {
+
+        NotaFiscalResponseDTO notaNova = notaFiscalService.emitirNota(request);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(notaNova.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(notaNova);
     }
 
 }
