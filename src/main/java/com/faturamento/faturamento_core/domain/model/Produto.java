97958ffabb8b38364600602daa37d393103cb3f1
brunophg <brunophgjf@gmail.com>
2026-08-20 10:39:39 -0300

    Add Product entity

---
+++ src/main/java/com/faturamento/faturamento_core/domain/model/Produto.java
@@ -0,0 +1,74 @@
+package com.faturamento.faturamento_core.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco", nullable = false)
+    private Double preco;
+
+    @Column(nullable = false)
+    private Boolean ativo = true;
+
+    public Produto() {
+
+    }
+
+    public Produto(String nome, String descricao, Double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.ativo = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
