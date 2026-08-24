7cedd6ab9e9bbf7aace0ef24d16fac107f42e1a4
brunophg <brunophgjf@gmail.com>
2026-08-24 12:19:26 -0300

    feat: create ProdutoController

--- src/main/java/com/faturamento/faturamento_core/api/controller/ProdutoController.java
+++ src/main/java/com/faturamento/faturamento_core/api/controller/ProdutoController.java
@@ -1,4 +1,51 @@
 package com.faturamento.faturamento_core.api.controller;
 
+import com.faturamento.faturamento_core.domain.dto.produto.ProdutoRequestDTO;
+import com.faturamento.faturamento_core.domain.dto.produto.ProdutoResponseDTO;
+import com.faturamento.faturamento_core.domain.dto.produto.ProdutoUpdateDTO;
+import com.faturamento.faturamento_core.domain.service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponseDTO>> listarTodos() {
+        List<ProdutoResponseDTO> produtos = produtoService.listarTodos();
+
+        return ResponseEntity.ok().body(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> buscarPorId(@PathVariable long id) {
+        ProdutoResponseDTO produto = produtoService.buscarPorId(id);
+
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDTO> adicionar(@RequestBody @Valid ProdutoRequestDTO request, UriComponentsBuilder uriBuilder) {
+        ProdutoResponseDTO produto = produtoService.adicionarProduto(request);
+        var uri = uriBuilder.path("/produtos/{id}").buildAndExpand(produto.id()).toUri();
+
+        return ResponseEntity.created(uri).body(produto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> atualizar(@PathVariable long id, @RequestBody @Valid ProdutoUpdateDTO request) {
+        ProdutoResponseDTO produto = produtoService.atualizarProduto(id, request);
+        return ResponseEntity.ok(produto);
+    }
 }
